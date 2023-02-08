a4c59e78bfa876425ac063cafcb0481604c682ca
Marianopiv <marianopividori93@gmail.com>
2023-02-08 00:47:10 -0300

    toco el loading

--- src/components/protectedRoute/ProtectedRoute.tsx
+++ src/components/protectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../../context/Provider'
+import "../../App.css"
 
 type Props = {}
 
 export const ProtectedRoute = ({children}:any) => {
   const {user,loading} = useAuth()
 
-  if (loading) return <h1>loading</h1>
+  if (loading) return <div className="lds-spinner 'h-screen'"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
   if (!user) return <Navigate to ='/login'/>
   return <>{children}</>
 }
