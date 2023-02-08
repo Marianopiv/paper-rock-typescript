9a210770386caa80e3c6f406c5063bab6eb18eb0
Marianopiv <marianopividori93@gmail.com>
2023-02-08 16:10:10 -0300

    meto responsive al highscore

--- src/components/highScore/HighScore.tsx
+++ src/components/highScore/HighScore.tsx
@@ -10,8 +10,8 @@ type Props = {
 const HighScore = ({setModal,fullUsers}: Props) => {
 
   return (
-    <div className="text-4xl text-white absolute inset-0 flex items-center justify-center h-screen w-screen bg-black z-50 opacity-80 animate__fadeOut">
-      <div className="border-2 border-white rounded-md p-3">
+    <div className="text-lg sm:text-4xl text-white absolute inset-0 flex items-center justify-center h-screen  sm:w-screen bg-black z-50 opacity-80 animate__fadeOut">
+      <div className="border-2 border-white rounded-md p-3 m-4">
         {" "} <p onClick={()=>setModal(false)} className="hover:cursor-pointer">X</p>
         <h1>HIGH SCORES</h1>
         {fullUsers.length>0&&getFiveHigher(fullUsers).map(({ email, highscore },index) => (
